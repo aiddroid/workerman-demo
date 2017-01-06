f227630fb2085f6f152b94c483c73a85224e0a37
aiddroid <aiddroid@gmail.com>
2017-01-06 17:52:52 +0800

    feat:add comments

--- http_server.php
+++ http_server.php
@@ -6,9 +6,11 @@ use Workerman\Worker;
 
 //setup worker
 $worker = new Worker('http://0.0.0.0:8801');
+//set worker number
 $worker->count = 4;
+//set onMessage handler
 $worker->onMessage = function($connection, $data){
     $connection->send('hello world!');
 };
-
+//run worker
 Worker::runAll();
