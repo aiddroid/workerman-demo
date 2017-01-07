5e5ebde939fb42cd70a8ea2b696025adb30b926c
aiddroid <aiddroid@gmail.com>
2017-01-07 12:23:56 +0800

    add comments

--- tcp_server.php
+++ tcp_server.php
@@ -6,9 +6,11 @@ use Workerman\Worker;
 
 //setup worker
 $worker = new Worker('tcp://0.0.0.0:8801');
+//setup worker number
 $worker->count = 4;
+//setup onMessage handler
 $worker->onMessage = function($connection, $data){
     $connection->send(strrev($data));
 };
-
+//run worker
 Worker::runAll();
